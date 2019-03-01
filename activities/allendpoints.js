7c5b08e904f1de011b2224edc50f1f06b04aaa96
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-01 13:10:26 +0100

    initial commit.

---
+++ activities/allendpoints.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
+
+module.exports = async function (activity) {
+
+  try {
+    api.initialize(activity);
+
+    const response = await api('');
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
+    // convert response to items[]
+    activity.Response.Data = api.convertResponse(response);
+  } catch (error) {
+
+    cfActivity.handleError(error, activity);
+  }
+};
