541134d407e8729bf12046c84f1b90f81636cdfc
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-05 15:22:12 +0100

    added activity 'myleads-status.js'.

---
+++ activities/myleads-status.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
+
+module.exports = async (activity) => {
+  try {
+    api.initialize(activity);
+
+    const response = await api('/v26.0/sobjects/lead');
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
+
+    let leads = [];
+    if (response.body.recentItems) {
+      leads = response.body.recentItems;
+    }
+
+    let salesforceDomain = api.getDomain();
+
+    let leadsStatus = {
+      title: 'Active Leads',
+      url: `https://${salesforceDomain}/lightning/o/Lead/list`,
+      urlLabel: 'All Leads',
+    };
+
+    let leadsCount = leads.length;
+
+    if (leadsCount != 0) {
+      leadsStatus = {
+        ...leadsStatus,
+        description: `You have ${leadsCount > 1 ? leadsCount + " leads" : leadsCount + " lead"}.`,
+        color: 'blue',
+        value: leadsCount,
+        actionable: true
+      };
+    } else {
+      leadsStatus = {
+        ...leadsStatus,
+        description: `You have no leads.`,
+        actionable: false
+      };
+    }
+
+    activity.Response.Data = leadsStatus;
+  } catch (error) {
+    cfActivity.handleError(activity, error);
+  }
+};
